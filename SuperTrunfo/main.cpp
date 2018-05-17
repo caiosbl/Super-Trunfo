e80d6644a4c511a05608161833225d3690374e83
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-05-17 15:52:44 -0300

    Inicialização do Modelo

--- SuperTrunfo/main.cpp
+++ SuperTrunfo/main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+struct carta1 {
+int ataque;
+int defesa;
+int meio;
+int titulos;
+int aparicao_copas;
+};
+
+// Um Struct para cada Seleção
+
 int main()
+
+
 {
     cout << "Hello world!" << endl;
     return 0;
