1d745d5d0a8708dcf9d7103676d781898c81302d
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-05-17 19:20:04 -0300

    Definido struct de carta

--- SuperTrunfo/main.cpp
+++ SuperTrunfo/main.cpp
@@ -2,22 +2,32 @@
 
 using namespace std;
 
-struct Carta{
+typedef struct{
 string tipo;
+string nome;
 int ataque;
 int defesa;
 int meio;
 int titulos;
 int aparicao_copas;
 bool is_super_trunfo;
-};
+}Carta;
 
+Carta cartas [32];
 
+void inicializar_cartas();
+
+void inicializar_cartas(){
+cartas[0].nome = "Brasil";
+cartas[0].ataque = 5;
+cartas[0].defesa = 10;
+cartas[0].meio = 4;
+cartas[0].titulos = 5;
+cartas[0].aparicao_copas = 4;
+}
 
 int main()
-
-
-{
-    cout << "Hello world!" << endl;
+{   inicializar_cartas();
+    cout << "Hello world!" << cartas[0] << endl;
     return 0;
 }
