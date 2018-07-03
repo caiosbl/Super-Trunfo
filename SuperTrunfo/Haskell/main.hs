98c54d41d48d9c74edc3603af2dd4da6bee7b6f8
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-07-03 20:29:01 -0300

    TypeClass de Carta adicionado

--- SuperTrunfo/Haskell/main.hs
+++ SuperTrunfo/Haskell/main.hs
@@ -1,1 +1,15 @@
-﻿
+﻿data Carta = Carta { tipo :: String  
+                     , nome :: String  
+                     , ataque :: Int  
+                     , defesa :: Int
+                     , meio :: Int
+                     , titulos :: Int
+                     , aparicoes_copas :: Int
+                     , is_trunfo :: Bool
+                     } deriving (Show)  
+
+
+main :: IO()
+
+main = do 
+    
