b3dea40cf25b30e92f8c50b1edcb9dd3d5bcb870
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-07-03 21:24:39 -0300

    Adicionado toString de Carta

--- SuperTrunfo/Haskell/main.hs
+++ SuperTrunfo/Haskell/main.hs
@@ -8,8 +8,21 @@
                      , is_trunfo :: Bool
                      } deriving (Show)  
 
-
+toStringCarta :: Carta -> String
+toStringCarta (Carta {tipo = tip, nome = nom, ataque = ata,
+ defesa = def, meio = mei, titulos = tit, aparicoes_copas = apa, is_trunfo = is}) = 
+    "Tipo: " ++ tip ++ "\n" ++
+                       "Selecao: " ++ nom ++ "\n" ++
+                       "Ataque: " ++ show(ata) ++ "\n" ++
+                       "Meio: " ++ show(mei) ++ "\n" ++
+                       "Defesa: " ++ show(def) ++ "\n" ++
+                       "Titulos: " ++ show(tit) ++ "\n" ++
+                       "Aparicoes em Copas: " ++  show(apa) ++ "\n" ++
+                       if is then "É TRUNFO" else ""
 main :: IO()
 
 main = do 
-    
+  let carta = Carta {tipo = "A1", nome = "Brasil", ataque = 87, defesa = 86, meio = 50,
+  titulos = 6, aparicoes_copas = 7, is_trunfo = True}
+   
+  putStrLn (toStringCarta carta)
